89b5f655c6aa10ad2a92445242531b049dcc6061
tlbravo <tbravo507@gmail.com>
2024-09-12 10:22:04 -0400

    updates

--- cheese_hc.R
+++ cheese_hc.R
@@ -4,12 +4,7 @@ install.packages("cluster")
 install.packages("fpc")
 install.packages("factoextra")
 install.packages("janitor")
-install.packages("here")
-install.packages("fs")
-install.packages("here")
-install.packages("rvest")
-install.packages("polite")
-install.packages("glue")
+install.packages("klaR")
 
 #load libraries
 library(tidyverse)
@@ -17,12 +12,23 @@ library(cluster)
 library(fpc)
 library(factoextra)
 library(janitor)
-library(here)
-library(fs)
-library(rvest)
-library(polite)
-library(glue)
+library(klaR)
+
 
 #reading data from github
 cheeses <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-06-04/cheeses.csv')
 View(cheeses)
+
+#selecting categorical columns 
+cheesedf<-cheeses[c(1,3,4,7, 13)]
+View(cheesedf)
+
+#using k-modes method 
+kmodes <- kmodes(cheesedf, modes = 3, iter.max = 10, weighted = FALSE)
+
+#adding cluster assignments
+kmodes$cluster  
+kmodes$modes 
+
+#adding to dataset
+cheesedf$cluster <- kmodes$cluster
